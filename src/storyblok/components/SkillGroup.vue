e386ef5440dc5c877c8daccb418777b217e823d3
vonByte <develplusdesign@gmail.com>
2020-05-26 00:12:41 +0200

    Personal honeycomb

--- src/storyblok/components/SkillGroup.vue
+++ src/storyblok/components/SkillGroup.vue
@@ -6,20 +6,16 @@
                 <skill-entry v-for="skill in group.entries" :key="skill._uid" :skill="skill"/>
             </div>
         </template>
-        <div v-else class="personal__wrapper is-flex">
-            <div class="personal active">
-                <div class="personal__title">
-                    <span>Personal Skills</span>
-                </div>
-                <div class="clouds">
-                    <div v-for="skill in group.entries" :key="skill._uid" class="personal__rotator">
-                        <div class="personal__cloud">
-                            <div class="personal__content">{{skill.title}}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
+        <div v-else class="personal">
+            <ul class="hc">
+                <li v-for="skill in group.entries" :key="skill._uid" class="hc-cell">
+                    <div class="hc-cell__title">{{skill.title}}</div>
+                </li>
+
+                <li class="hc-cell hc__placeholder"></li>
+
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -50,95 +46,136 @@ export default {
         }
     }
 
-    .absolute-center {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translateX(-50%) translateY(-50%);
-    }
+    $cell-width: 120px;
+    $cell-height: $cell-width*.55;
+    $cell-margin: 15px;
+    $grid-max-width: 800px;
+    $viewport-thereshold: $cell-width + $cell-margin;
+
     .personal {
-        &__wrapper {
-            width: 100%;
-            justify-content: center;
-            align-items: center;
-            margin-top: 18em;
-        }
-        width: 8rem;
-        height: 8rem;
-        position: relative;
-
-        &__title {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            transform: translateX(0);
+        display: flex;
+        justify-content: center;
+        .hc {
             display: flex;
+            flex-wrap: wrap;
             justify-content: center;
             align-items: center;
-            text-align: center;
-            > span {
-                font-size: 1.5rem;
-                border-bottom: 3px solid $primary;
-                border-top: 3px solid $primary;
+            max-width: $grid-max-width;
+            transform: translateY(#{$cell-height/4});
+
+            &-cell {
+                flex: 0 1 $cell-width;
+                max-width: $cell-width;
+                height: $cell-height;
+                margin: $cell-height/2.1 $cell-margin/2 $cell-margin;
                 position: relative;
-                &:after,
-                &:before {
-                    display: block;
-                    background: $primary;
-                    content: '';
-                    width: 80%;
-                    height: 3px;
-                    position: absolute;
-                    top: -10px;
-                    left: 10%;
-                }
-                &::after {
-                    top: auto;
-                    bottom: -10px;
-                }
-            }
-        }
-
-        &__rotator {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            transform-origin: 50% 50%;
-        }
-
-            .personal__cloud {
+                padding: 0.5em;
                 text-align: center;
-                padding: 1.2rem;
-                opacity: 1;
-                z-index: 50;
-                border-radius: 50%;
-                width: 8rem;
-                height: 8rem;
-                background: $primary;
-                font-size: 0.9em;
-                display: flex;
-                justify-items: center;
-                align-items: center;
+                transition: transform 0.3s ease-in;
+
+                &__title {
+                    height: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    hyphens: auto;
+                    word-break: break-word;
+                    color: $black;
+                    font-size: 0.85rem;
+                    transition: opacity 350ms;
+                }
+
+                &::before, &::after {
+                    content: '';
+                    top: -50%;
+                    left: 0;
+                    width: 100%;
+                    height: 200%;
+                    display: block;
+                    position: absolute;
+                    clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+                    z-index: -1;
+                }
+
+                &::before {
+                    background: $primary;
+                    transform: scale(1.02);
+                }
+
+                &::after {
+                    background: $white;
+                    opacity: 1;
+                    transition: opacity 350ms;
+                    transform: scale(0.98);
+                }
+
+                &:hover {
+                    transform: scale(1.1);
+                    cursor: default;
+                    transition: transform 0.3s ease-out;
+                }
             }
-        .personal__content {
+
+            &__placeholder{
+                display:none;
+                opacity: 0;
+                width: $cell-width;
+                margin: 0 $cell-margin/2;
+            }
 
         }
-    }
+        @media (max-width:#{2*$viewport-thereshold}){
+            .hc-cell {
+                margin: #{$cell-margin/2 + $cell-height/2} $cell-margin;
+            }
+        }
 
-    $numOfElems: 13;
+        @media (min-width: #{2*$viewport-thereshold}) and (max-width:#{3*$viewport-thereshold}){
+            .hc-cell:nth-child(3n) {
+                margin-right: calc(50% - #{$cell-width/2});
+                margin-left: calc(50% - #{$cell-width/2});
+            }
 
-    @for $i from 1 through $numOfElems {
-        $deg: 360deg / $numOfElems;
-        .personal__rotator:nth-child(#{$i}) {
-            transform: rotate($deg/-2 + $deg * ($i - 1));
-            .personal__cloud {
-                transform: translateY(-14rem) rotate($deg/2 - $deg * ($i - 1));
+            .hc__placeholder:nth-child(3n + 5) {
+                display:block;
+            }
+        }
+
+        @media (min-width: #{3*$viewport-thereshold}) and (max-width:#{4*$viewport-thereshold}) {
+            .hc-cell:nth-child(5n + 4) {
+                margin-left: calc(50% - #{$cell-width + $cell-margin});
+            }
+
+            .hc-cell:nth-child(5n + 5) {
+                margin-right: calc(50% - #{$cell-width + $cell-margin});
+            }
+
+            .hc__placeholder:nth-child(5n), .hc__placeholder:nth-child(5n + 3) {
+                display:block;
+            }
+        }
+
+        @media (min-width: #{4*$viewport-thereshold}) {
+            .hc-cell:nth-child(7n + 5)  {
+                margin-left: calc(50% - #{$cell-width*1.5 + $cell-margin});
+            }
+
+            .hc-cell:nth-child(7n + 7), .hc-cell:nth-child(7n + 5):nth-last-child(2) {
+                margin-right: calc(50% - #{$cell-width*1.5 + $cell-margin});
+            }
+
+            .hc__placeholder:nth-child(7n + 7), .hc__placeholder:nth-child(7n + 9), .hc__placeholder:nth-child(7n + 11){
+                display:block;
             }
         }
     }
+    .honeycomb {
+
+
+
+    }
+
+
+
 
 </style>
